cfa9e5e90524a3b749f7c5ceef1d671ab0debf96
spring-raining <harusamex.com@gmail.com>
2020-04-16 02:40:31 +0900

    Adopt firebase auth

--- web/component/header.tsx
+++ web/component/header.tsx
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from '../services/firebase';
 import * as UI from './ui';
 
-export const Header = () => (
-  <UI.Box w="100%" backgroundColor="gray.200">
-    <UI.Container py={2} justify="space-between" align="center">
+const signIn = async () => {
+  const provider = new firebase.auth.GithubAuthProvider();
+  await firebase.auth().signInWithRedirect(provider);
+};
+
+const signOut = async () => {
+  await firebase.auth().signOut();
+};
+
+const useAuthorizedUser = () => {
+  const [isPending, setPending] = useState(true);
+  const [user, setUser] = useState<firebase.User | null>(null);
+
+  useEffect(() => {
+    return firebase.auth().onAuthStateChanged((user) => {
+      setPending(false);
+      setUser(user);
+    });
+  }, []);
+
+  return { user, isPending };
+};
+
+const HeaderUserInfo: React.FC = () => {
+  const { user, isPending } = useAuthorizedUser();
+  if (isPending) {
+    return null;
+  }
+  return (
+    <>
+      {user && <UI.Text mr={2}>logged in as {user.displayName}</UI.Text>}
+      {user && (
+        <UI.Button
+          variant="outline"
+          variantColor="blackAlpha"
+          onClick={signOut}
+        >
+          Logout
+        </UI.Button>
+      )}
+      {!user && (
+        <UI.Button variant="outline" variantColor="blackAlpha" onClick={signIn}>
+          Login
+        </UI.Button>
+      )}
+    </>
+  );
+};
+
+export const Header: React.FC = () => (
+  <UI.Flex w="100%" h={16} backgroundColor="gray.200">
+    <UI.Container w="100%" justify="space-between" align="center">
       <UI.Heading size="sm">Vivliostyle Pub</UI.Heading>
-      <UI.Button variant="outline" variantColor="blackAlpha">
-        Login
-      </UI.Button>
+      <UI.Flex align="center">
+        <HeaderUserInfo />
+      </UI.Flex>
     </UI.Container>
-  </UI.Box>
+  </UI.Flex>
 );
--- web/next.config.js
+++ web/next.config.js
@@ -1,1 +1,15 @@
-module.exports = {};
+require('dotenv').config({ path: '../.env' });
+
+const checkPublicEnv = (envName) => {
+  return envName.startsWith('FIREBASE');
+};
+const env = Object.keys(process.env).reduce((acc, key) => {
+  if (checkPublicEnv(key)) {
+    acc[key] = process.env[key];
+  }
+  return acc;
+}, {});
+
+module.exports = {
+  env,
+};
