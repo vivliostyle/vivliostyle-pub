653e58a867a86a12b311c7bb60bfeb41d8416dac
spring-raining <harusamex.com@gmail.com>
2020-04-26 13:58:02 +0900

    Add setup of firebase admin

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,16 @@
 require('dotenv').config({ path: '../.env' });
 
 const checkPublicEnv = (envName) => {
-  return (
-    envName.startsWith('FIREBASE') || envName == 'GITHUB_APP_INSTALLATION_URL'
-  );
+  return [
+    'FIREBASE_API_KEY',
+    'FIREBASE_AUTH_DOMAIN',
+    'FIREBASE_DATABASE_URL',
+    'FIREBASE_PROJECT_ID',
+    'FIREBASE_STORAGE_BUCKET',
+    'FIREBASE_MESSAGING_SENDER_ID',
+    'FIREBASE_APP_ID',
+    'GITHUB_APP_INSTALLATION_URL',
+  ].includes(envName);
 };
 const env = Object.keys(process.env).reduce((acc, key) => {
   if (checkPublicEnv(key)) {
