851f67e99f442e162559809bcd50f4143aed713c
spring-raining <harusamex.com@gmail.com>
2020-05-04 23:25:28 +0900

    Create requestSession API

--- web/components/MarkdownEditor.tsx
+++ web/components/MarkdownEditor.tsx
@@ -1,5 +1,44 @@
-import Editor from '@monaco-editor/react';
+import React, { useCallback, useState, useEffect } from 'react';
+import Editor, { EditorDidMount } from '@monaco-editor/react';
 
-export const MarkdownEditor = () => (
-  <Editor height="80vh" language="markdown" />
-);
+export const MarkdownEditor = ({
+  value = '',
+  onModified = () => {},
+  onUpdate = () => {},
+}: {
+  value?: string;
+  onModified?: (value: string) => void;
+  onUpdate?: (value: string) => void;
+}) => {
+  const [currentValue, setCurrentValue] = useState(() => value);
+  useEffect(() => {
+    const id = setTimeout(() => {
+      onUpdate(currentValue);
+    }, 3000);
+    return () => {
+      clearTimeout(id);
+    };
+  }, [currentValue]);
+  const editorDidMount: EditorDidMount = useCallback(
+    (getEditorValue, monaco) => {
+      monaco.onDidChangeModelContent(() => {
+        const value = getEditorValue();
+        setCurrentValue(value);
+        onModified(value);
+      });
+    },
+    []
+  );
+
+  return (
+    <Editor
+      height="80vh"
+      language="markdown"
+      value={value}
+      options={{
+        minimap: { enabled: false },
+      }}
+      editorDidMount={editorDidMount}
+    />
+  );
+};
--- web/pages/github/[owner]/[repo].tsx
+++ web/pages/github/[owner]/[repo].tsx
@@ -1,9 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import { useRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
 import { Header } from '../../../components/Header';
 import { MarkdownEditor } from '../../../components/MarkdownEditor';
 import * as UI from '../../../components/ui';
 import { useAuthorizedUser } from '../../../middlewares/useAuthorizedUser';
+
+const useEditorSession = ({
+  owner,
+  repo,
+  user,
+}: {
+  owner: string;
+  repo: string;
+  user?: firebase.User;
+}) => {
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+    (async () => {
+      const idToken = await user.getIdToken();
+      const ret = await fetch('/api/github/requestSession', {
+        method: 'POST',
+        body: JSON.stringify({ owner, repo }),
+        headers: {
+          'content-type': 'application/json',
+          'x-id-token': idToken,
+        },
+      }).then((r) => r.json());
+      console.log(ret);
+    })();
+  }, [owner, repo, user]);
+};
 
 export default () => {
   const { user, isPending } = useAuthorizedUser();
@@ -15,12 +44,43 @@ export default () => {
     }
   }, [user, isPending]);
 
-  // const { owner, repo } = router.query;
+  const { owner, repo } = router.query;
+  useEditorSession({
+    user,
+    owner: Array.isArray(owner) ? owner[0] : owner,
+    repo: Array.isArray(repo) ? repo[0] : repo,
+  });
+
+  const [text, setText] = useState('');
+  const [status, setStatus] = useState<'init' | 'modified' | 'saved'>('init');
+  const onModified = useCallback(() => {
+    setStatus('modified');
+  }, []);
+  const onUpdate = useCallback(
+    (updatedText) => {
+      if (updatedText === text) {
+        return;
+      }
+      setText(updatedText);
+      setStatus('saved');
+    },
+    [text]
+  );
   return (
     <UI.Box>
       <Header />
+      <UI.Flex
+        w="100%"
+        h={12}
+        borderBottomWidth={1}
+        borderBottomColor="gray.300"
+      >
+        <UI.Flex w="100%" px={8} justify="flex-start" align="center">
+          {status === 'saved' && <UI.Text>Document updated</UI.Text>}
+        </UI.Flex>
+      </UI.Flex>
       {!isPending ? (
-        <MarkdownEditor />
+        <MarkdownEditor {...{ onModified, onUpdate }} />
       ) : (
         <UI.Container mt={6}>
           <UI.Text>Loading</UI.Text>
