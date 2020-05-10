24c9aa18a54091bfe13e18e8288605cd0a07cb96
takanakahiko <takanakahiko@gmail.com>
2020-05-10 19:02:59 +0900

    refactoring

--- cloud-run/scripts/createTopic.js
+++ cloud-run/scripts/createTopic.js
@@ -1,20 +1,13 @@
 'use strict';
 
-const {PubSub} = require('@google-cloud/pubsub');
-
-async function main(topicName) {
+const main = async(topicName, subscriptionName) => {
+  const {PubSub} = require('@google-cloud/pubsub');
   const pubSubClient = new PubSub();
   const [topic] = await pubSubClient.createTopic(topicName);
-  const createPushSubscription = async() => {
-    const options = { pushConfig: {pushEndpoint: `http://localhost:8080/`,},};
-    const subscriptionName = 'test';
-    await pubSubClient.topic(topicName).createSubscription(subscriptionName, options);
-    console.log(`Subscription ${subscriptionName} created.`);
-  }
-  await createPushSubscription().catch(console.error);
+  console.log(`>> Topic ${topicName} created.`);
+  const options = { pushConfig: {pushEndpoint: `http://localhost:8080/`,},};
+  await pubSubClient.topic(topicName).createSubscription(subscriptionName, options);
+  console.log(`>> Subscription ${subscriptionName} created.`);
 }
 
-
-(async() => {
-  await main(...process.argv.slice(1));
-})();
+main(...process.argv.slice(2)).then(process.exit).catch(console.error);
--- cloud-run/scripts/publishMessage.js
+++ cloud-run/scripts/publishMessage.js
@@ -1,18 +1,10 @@
 'use strict';
 
-const {PubSub} = require('@google-cloud/pubsub');
-
-async function main(topicName,data) {
+const main = async(topicName, data) => {
+  const { PubSub } = require('@google-cloud/pubsub');
   const pubSubClient = new PubSub();
-  await createPushSubscription().catch(console.error);
-  const publishMessage = async() => {
-    const dataBuffer = Buffer.from(data);
-    const messageId = await pubSubClient.topic(topicName).publish(dataBuffer);
-    console.log(`Message ${messageId} published.`);
-  }
-  await publishMessage().catch(console.error);
+  const messageId = await pubSubClient.topic(topicName).publish(Buffer.from(data));
+  console.log(`>> Message ${messageId} published.`);
 }
 
-(async() => {
-  await main(...process.argv.slice(2));
-})();
+main(...process.argv.slice(2)).then(process.exit).catch(console.error);
