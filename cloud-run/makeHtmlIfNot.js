922ad1918d3fbb6d4eb810fdcee4d934e3e5a79c
takanakahiko <takanakahiko@gmail.com>
2020-05-16 11:05:51 +0900

    make html if not

---
+++ cloud-run/makeHtmlIfNot.js
@@ -0,0 +1,41 @@
+const unified = require('unified');
+const markdown = require('remark-parse');
+const remark2rehype = require('remark-rehype');
+const raw = require('rehype-raw');
+const doc = require('rehype-document');
+const stringify = require('rehype-stringify');
+
+const {rubyParser, rubyHandler} = require('./ruby');
+
+function makeHtmlIfNot(
+  {stylesheet = ''}
+){
+  const fs = require('fs');
+  try {
+    fs.accessSync('index.html');
+  } catch (error) {
+    console.log('>> index.html was not found, so create index.html from index.md');
+    const markdownString = fs.readFileSync("index.md");
+    const htmlString = stringifyMarkdown( markdownString, { stylesheet })
+    fs.writeFileSync("index.html", htmlString);
+  }
+}
+
+function stringifyMarkdown(
+  markdownString, {stylesheet = ''}
+) {
+  const processor = unified()
+    .use(markdown, {commonmark: true})
+    .use(rubyParser)
+    .use(remark2rehype, {
+      allowDangerousHTML: true,
+      handlers: {ruby: rubyHandler},
+    })
+    .use(raw)
+    .use(doc, {language: 'ja', css: stylesheet})
+    .use(stringify);
+  const generated = String(processor.processSync(markdownString));
+  return generated;
+}
+
+module.exports = makeHtmlIfNot
