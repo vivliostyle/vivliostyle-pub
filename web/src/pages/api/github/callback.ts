91602c2f461b786c79a1146006afd6f34948cba7
takanakahiko <takanakahiko@gmail.com>
2021-08-15 15:23:17 +0900

    migrate from github app install callback to oauth callback

--- web/src/pages/api/github/callback.ts
+++ web/src/pages/api/github/callback.ts
@@ -6,17 +6,9 @@ import firebaseAdmin from '@services/firebaseAdmin';
 import {encrypt} from '@utils/encryption';
 
 const installation: NextApiHandler = async (req, res) => {
-  const installationId = +req.query['installation_id'];
-  const setupAction = req.query['setup_action'];
   const code = req.query['code'];
-  if (!(installationId > 0) || !code) {
-    res.writeHead(302, {
-      Location: process.env.GH_APP_INSTALLATION_URL,
-    });
-    return res.end();
-  }
-  if (setupAction !== 'install' && setupAction !== 'update') {
-    return res.status(400).send(null);
+  if (!code) {
+    return res.status(500).send("parameter code not found");
   }
 
   const response = await fetch('https://github.com/login/oauth/access_token', {
@@ -47,8 +39,6 @@ const installation: NextApiHandler = async (req, res) => {
     await firebaseAdmin.auth().setCustomUserClaims(user.uid, {
       githubAccessToken: encrypted,
     });
-    // Revoke token to renew user claims
-    await firebaseAdmin.auth().revokeRefreshTokens(user.uid);
   } catch (e) {}
   res.writeHead(302, {Location: '/'});
   return res.end();
