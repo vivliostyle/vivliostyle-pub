a719ad6601bcaf0b4d26c9092f61d2015f4d8369
Ayumu Takai <atakai@hackwork.jp>
2021-10-07 04:23:30 +0900

    fix: cached files give 404 Not found error

--- web/public/worker/serviceWorker.js
+++ web/public/worker/serviceWorker.js
@@ -10,12 +10,6 @@ self.addEventListener('fetch', function (event) {
         return response;
       } else {
         return fetch(event.request).then(function (response) {
-          // Response can be used only once:
-          // we have to make a clone to the response and put the first copy to the cache, then return the last copy to the client.
-          let responseClone = response.clone();
-          caches.open(CACHE_NAME).then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
           return response;
         });
       }
