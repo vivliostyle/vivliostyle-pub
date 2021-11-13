d0e15ddd364d4c0daf12c91ff12d10600c099f0b
Ayumu Takai <atakai@hackwork.jp>
2021-11-13 13:29:39 +0900

    .devファイルが無い場合にエラー表示して終了する

--- web/src/services/firebase.ts
+++ web/src/services/firebase.ts
@@ -4,6 +4,10 @@ import 'firebase/firestore';
 import 'firebase/functions';
 
 if (!firebase.apps.length) {
+  if(! process.env.FIREBASE_CONFIG) {
+    console.error('file not found: .env');
+    process.exit(-1);
+  }
   const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
   firebase.initializeApp(firebaseConfig);
 }
