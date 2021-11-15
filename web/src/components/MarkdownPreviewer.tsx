d486d0be1af187dbcb0d78b286ae138d3d5da3b4
Ayumu Takai <atakai@hackwork.jp>
2021-11-16 02:01:39 +0900

    feat: add browser-default theme

--- web/src/components/MarkdownPreviewer.tsx
+++ web/src/components/MarkdownPreviewer.tsx
@@ -55,7 +55,7 @@ const updateCacheFromPath = async(owner: string, repo: string, basePath: string,
 interface PreviewerProps {
   body: string;
   basename: string;
-  stylesheet?: string;
+  stylesheet?: string|null;
   owner: string;
   repo: string;
   user: firebase.User | null;
