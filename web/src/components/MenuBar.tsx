65b613866f3ec2a1d648ffb068a9a0d208487b7e
Ayumu Takai <atakai@hackwork.jp>
2021-11-18 13:06:05 +0900

    ソースコードの整理

--- web/src/components/MenuBar.tsx
+++ web/src/components/MenuBar.tsx
@@ -1,14 +1,17 @@
 import React, {Dispatch, useCallback, useEffect, useState} from 'react';
 import * as UI from '@components/ui';
-import { BranchSelecter } from './BranchSelecter';
-import { CommitSessionButton } from './CommitSessionButton';
-import { FileUploadModal } from './FileUploadModal';
-import { FileState, useRepositoryContext } from '@middlewares/useRepositoryContext';
-import { useAppContext } from '@middlewares/useAppContext';
+import {BranchSelecter} from './BranchSelecter';
+import {CommitSessionButton} from './CommitSessionButton';
+import {FileUploadModal} from './FileUploadModal';
+import {
+  FileState,
+  useRepositoryContext,
+} from '@middlewares/useRepositoryContext';
+import {useAppContext} from '@middlewares/useAppContext';
 
 import {Theme, ThemeManager} from 'theme-manager';
-import { usePreviewSourceContext } from '@middlewares/usePreviewSourceContext';
-import { useDisclosure } from '@chakra-ui/react';
+import {usePreviewSourceContext} from '@middlewares/usePreviewSourceContext';
+import {useDisclosure} from '@chakra-ui/react';
 
 const GitHubAccessToken: string | null =
   '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';
@@ -18,46 +21,46 @@ const themeManager = new ThemeManager(GitHubAccessToken);
 export function MenuBar({
   status,
   isProcessing,
-    isPresentationMode,
-    setPresentationMode,
-    setStatus,
-    setWarnDialog,
-    onBuildPDFButtonClicked
-}:{
-    status:FileState;
-    isProcessing:boolean;
-    isPresentationMode:boolean;
-    setPresentationMode:Dispatch<React.SetStateAction<boolean>>;
-    setStatus:Dispatch<React.SetStateAction<FileState>>;
-    setWarnDialog:Dispatch<React.SetStateAction<boolean>>;
-    onBuildPDFButtonClicked:()=>void;
+  isPresentationMode,
+  setPresentationMode,
+  setStatus,
+  setWarnDialog,
+  onBuildPDFButtonClicked,
+}: {
+  status: FileState;
+  isProcessing: boolean;
+  isPresentationMode: boolean;
+  setPresentationMode: Dispatch<React.SetStateAction<boolean>>;
+  setStatus: Dispatch<React.SetStateAction<FileState>>;
+  setWarnDialog: Dispatch<React.SetStateAction<boolean>>;
+  onBuildPDFButtonClicked: () => void;
 }) {
-    const app = useAppContext();
-    const repository = useRepositoryContext();
-    const previewSource = usePreviewSourceContext();
+  const app = useAppContext();
+  const repository = useRepositoryContext();
+  const previewSource = usePreviewSourceContext();
 
-    const [themes, setThemes] = useState<Theme[]>([]);
+  const [themes, setThemes] = useState<Theme[]>([]);
 
-    useEffect(() => {
-        (async () => {
-          const themeList = await themeManager.searchFromNpm();
-          setThemes(themeList);
-        })();
-      }, [app.user]);
+  useEffect(() => {
+    (async () => {
+      const themeList = await themeManager.searchFromNpm();
+      setThemes(themeList);
+    })();
+  }, [app.user]);
 
-      const onDidSaved = useCallback(() => {
-        console.log('onDidSaved');
-        setStatus('clean');
-        setWarnDialog(false);
-      }, [setWarnDialog]);
+  const onDidSaved = useCallback(() => {
+    console.log('onDidSaved');
+    setStatus('clean');
+    setWarnDialog(false);
+  }, [setWarnDialog]);
 
-      const {
-        isOpen: isOpenFileUploadModal,
-        onOpen: onOpenFileUploadModal,
-        onClose: onCloseFileUploadModal,
-      } = useDisclosure();
+  const {
+    isOpen: isOpenFileUploadModal,
+    onOpen: onOpenFileUploadModal,
+    onClose: onCloseFileUploadModal,
+  } = useDisclosure();
 
-        /**
+  /**
    * スタイルシートが変更された
    * @param theme
    */
@@ -65,72 +68,70 @@ export function MenuBar({
     previewSource.changeTheme(theme);
   }
 
-    return (
-        <UI.Flex w="100%" px={8} justify="space-between" align="center">
-        <UI.Flex align="center">
-          {repository.owner} / {repository.repo} /
-          <UI.Box w="180px" px="4">
-            <BranchSelecter />
-          </UI.Box>
-          {/* {status === 'saved' && <UI.Text>Document updated : </UI.Text>} */}
-          {app.user /*&& session?.id*/ && (
-            <div>
-              <CommitSessionButton
-                {...{onDidSaved}}
-                disabled={!(status == 'saved' || status == 'modified')}
-              />
-              {status}
-            </div>
-          )}
-          {/* {stylesheet} */}
-        </UI.Flex>
-        <UI.Flex align="center">
-          {isProcessing && <UI.Spinner style={{marginRight: '10px'}} />}
-          <UI.Menu>
-            <UI.MenuButton as={UI.Button}>
-              <UI.Icon name="chevron-down" /> Actions
-            </UI.MenuButton>
-            <UI.MenuList>
-              <UI.MenuItem
-                key="presen"
-                onClick={() => {
-                  setPresentationMode(!isPresentationMode);
-                }}
-              >
-                {isPresentationMode ? '✔ ' : ' '}Presentation Mode
-              </UI.MenuItem>
-              <UI.MenuDivider />
-              <UI.MenuGroup title="Theme">
-                {themes.map((theme) => (
-                  <UI.MenuItem
-                    key={theme.name}
-                    onClick={() => onThemeSelected(theme)}
-                  >
-                    {theme.description}
-                  </UI.MenuItem>
-                ))}
-              </UI.MenuGroup>
-              <UI.MenuDivider />
-              <UI.MenuGroup title="Add Files">
-                <UI.MenuItem onClick={onOpenFileUploadModal}>
-                  Add Image
-                </UI.MenuItem>
-                <FileUploadModal
-                  user={app.user}
-                  isOpen={isOpenFileUploadModal}
-                  onOpen={onOpenFileUploadModal}
-                  onClose={onCloseFileUploadModal}
-                />
-              </UI.MenuGroup>
-              <UI.MenuDivider />
-              <UI.MenuGroup title="Export">
-                <UI.MenuItem onClick={onBuildPDFButtonClicked}>
-                  PDF
-                </UI.MenuItem>
-              </UI.MenuGroup>
-            </UI.MenuList>
-          </UI.Menu>
-        </UI.Flex>
+  return (
+    <UI.Flex w="100%" px={8} justify="space-between" align="center">
+      <UI.Flex align="center">
+        {repository.owner} / {repository.repo} /
+        <UI.Box w="180px" px="4">
+          <BranchSelecter />
+        </UI.Box>
+        {/* {status === 'saved' && <UI.Text>Document updated : </UI.Text>} */}
+        {app.user /*&& session?.id*/ && (
+          <div>
+            <CommitSessionButton
+              {...{onDidSaved}}
+              disabled={!(status == 'saved' || status == 'modified')}
+            />
+            {status}
+          </div>
+        )}
+        {/* {stylesheet} */}
       </UI.Flex>
-    );
+      <UI.Flex align="center">
+        {isProcessing && <UI.Spinner style={{marginRight: '10px'}} />}
+        <UI.Menu>
+          <UI.MenuButton as={UI.Button}>
+            <UI.Icon name="chevron-down" /> Actions
+          </UI.MenuButton>
+          <UI.MenuList>
+            <UI.MenuItem
+              key="presen"
+              onClick={() => {
+                setPresentationMode(!isPresentationMode);
+              }}
+            >
+              {isPresentationMode ? '✔ ' : ' '}Presentation Mode
+            </UI.MenuItem>
+            <UI.MenuDivider />
+            <UI.MenuGroup title="Theme">
+              {themes.map((theme) => (
+                <UI.MenuItem
+                  key={theme.name}
+                  onClick={() => onThemeSelected(theme)}
+                >
+                  {theme.description}
+                </UI.MenuItem>
+              ))}
+            </UI.MenuGroup>
+            <UI.MenuDivider />
+            <UI.MenuGroup title="Add Files">
+              <UI.MenuItem onClick={onOpenFileUploadModal}>
+                Add Image
+              </UI.MenuItem>
+              <FileUploadModal
+                user={app.user}
+                isOpen={isOpenFileUploadModal}
+                onOpen={onOpenFileUploadModal}
+                onClose={onCloseFileUploadModal}
+              />
+            </UI.MenuGroup>
+            <UI.MenuDivider />
+            <UI.MenuGroup title="Export">
+              <UI.MenuItem onClick={onBuildPDFButtonClicked}>PDF</UI.MenuItem>
+            </UI.MenuGroup>
+          </UI.MenuList>
+        </UI.Menu>
+      </UI.Flex>
+    </UI.Flex>
+  );
 }
