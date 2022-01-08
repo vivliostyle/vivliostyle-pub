9924d8e46f9718447f7bc4656552fd7ab623e82b
takanakahiko <takanakahiko@gmail.com>
2022-01-08 14:28:28 +0900

    write signedUrl with PDF build result

--- cloud-run/src/cloud-storage.ts
+++ cloud-run/src/cloud-storage.ts
@@ -6,14 +6,27 @@ const storage = new Storage();
 
 const bucketName = 'vivliostyle-pub-build-pdf';
 
-export async function uploadFile(name: string, path: string) {
+interface UploadFileResult {
+  signedUrl: string
+  url: string
+}
+
+export async function uploadFile(name: string, path: string): Promise<UploadFileResult> {
   const filename = `${name}-${uuid.v4()}.pdf`;
-  await storage.bucket(bucketName).upload(path, {
+  const ret = await storage.bucket(bucketName).upload(path, {
     gzip: true,
     metadata: {
       cacheControl: 'public, max-age=31536000',
     },
     destination: filename,
   });
-  return `https://storage.cloud.google.com/${bucketName}/${filename}?hl=ja`;
+  const signedUrl = await ret[0].getSignedUrl({
+    version: 'v4',
+    action: 'read',
+    expires: Date.now() + 15 * 60 * 1000,
+  })
+  return {
+    signedUrl: signedUrl[0],
+    url: `https://storage.cloud.google.com/${bucketName}/${filename}?hl=ja`;
+  }
 }
