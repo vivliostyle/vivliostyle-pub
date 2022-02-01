cda5f26cf98f242b77d82bb1d187a12c40e56235
Ayumu Takai <atakai@hackwork.jp>
2022-02-01 16:26:25 +0900

    ブランチを切り替えてもプレビューがそのまま表示される不具合修正

--- web/src/components/MarkdownPreviewer.tsx
+++ web/src/components/MarkdownPreviewer.tsx
@@ -1,7 +1,7 @@
 import {useRef, useEffect, useState, memo} from 'react';
 
 import {usePreviewSourceContext} from '@middlewares/contexts/usePreviewSourceContext';
-import { useCurrentThemeContext } from '@middlewares/contexts/useCurrentThemeContext';
+import {useCurrentThemeContext} from '@middlewares/contexts/useCurrentThemeContext';
 import React from 'react';
 
 const VIVLIOSTYLE_VIEWER_HTML_URL =
@@ -9,50 +9,55 @@ const VIVLIOSTYLE_VIEWER_HTML_URL =
 
 interface PreviewerProps {}
 
-export const Previewer: React.FC<PreviewerProps> = ({ }) => {
+export const Previewer: React.FC<PreviewerProps> = ({}) => {
   const currentTheme = useCurrentThemeContext();
   const previewSource = usePreviewSourceContext();
 
-  const [currentPath,setCurrentPath] = useState<string|null>(null);
+  const [currentPath, setCurrentPath] = useState<string | null>(null);
 
   const iframeRef = useRef<HTMLIFrameElement>(null);
 
-  useEffect(()=>{
-
+  useEffect(() => {
     const iframeWindow = iframeRef.current?.contentWindow;
-    if( ! iframeWindow){
+    if (!iframeWindow) {
       return;
     }
 
-    let url = `${VIVLIOSTYLE_VIEWER_HTML_URL}?${Date.now()}#x=${
-      previewSource.vpubPath
-    }`;
-    const stylePath = currentTheme.theme?.getStylePath()
-      ? '/vpubfs/'+currentTheme.theme?.getStylePath()
-      : null;
-    if (stylePath) {
-      url += `&style=${stylePath}`;
-    }else{
-      console.log('no stylesheet');
-    }
-    if(currentPath != previewSource.vpubPath){
+    if (previewSource.vpubPath == null) {
+      // 対象となるファイルが選択されていない
+      // 初期状態、ブランチ切り替えやリポジトリ切り替えでこの状態になる
+      iframeWindow.location.href = '/viewer/empty.html';
+    } else if (currentPath != previewSource.vpubPath) {
       // 対象のファイルが変更された
-      iframeWindow.location.href = previewSource.vpubPath ? url : VIVLIOSTYLE_VIEWER_HTML_URL + '#x=empty.html';
+      let url = `${VIVLIOSTYLE_VIEWER_HTML_URL}?${Date.now()}#x=${
+        previewSource.vpubPath
+      }`;
+      const stylePath = currentTheme.theme?.getStylePath()
+        ? '/vpubfs/' + currentTheme.theme?.getStylePath()
+        : null;
+      if (stylePath) {
+        url += `&style=${stylePath}`;
+      } else {
+        console.log('no stylesheet');
+      }
+      console.log('preview href', url);
+      iframeWindow.location.href = url;
       setCurrentPath(previewSource.vpubPath);
-    }else{
+    } else {
+      console.log('preview reload');
       // 対象のファイルは変わらず、テキストだけ変更された
       iframeWindow.location.reload();
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[previewSource, currentTheme.theme]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [previewSource, currentTheme.theme]);
 
-  return (
-    <ViewerFrame iframeRef={iframeRef}></ViewerFrame>
-  );
+  return <ViewerFrame iframeRef={iframeRef}></ViewerFrame>;
 };
 
 // iframeを作りなおすとページが先頭に戻ってしまうため、無駄なレンダリングを防ぐためにメモ化する
-type ViewerFrameProps = {iframeRef:any;};
-const ViewerFrame = memo<ViewerFrameProps>(function useViewerFrame({iframeRef}) {
+type ViewerFrameProps = {iframeRef: any};
+const ViewerFrame = memo<ViewerFrameProps>(function useViewerFrame({
+  iframeRef,
+}) {
   return <iframe ref={iframeRef} width="100%" height="100%"></iframe>;
 });
