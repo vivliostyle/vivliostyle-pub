fbc2590e2b7d712842e52d75886441f06940f9e6
Ayumu Takai <atakai@hackwork.jp>
2022-02-02 03:49:32 +0900

    不要なインポートの削除

--- web/src/components/BranchSelecter.tsx
+++ web/src/components/BranchSelecter.tsx
@@ -1,18 +1,16 @@
 import React, { useCallback } from 'react';
 import * as UI from './ui';
 import { useRepositoryContext } from '@middlewares/contexts/useRepositoryContext';
-import { useLogContext } from '@middlewares/contexts/useLogContext';
 import { useCurrentFileContext } from '@middlewares/contexts/useCurrentFileContext';
 import { FileState } from '@middlewares/frontendFunctions';
 import { t } from 'i18next';
 
 export const BranchSelecter = () => {
-  const log = useLogContext();
   const repository = useRepositoryContext();
   const currentFile = useCurrentFileContext();
 
   const onChangeBranch = useCallback((e: React.ChangeEvent<HTMLSelectElement>) => {
-    console.log('onChangeBranch',currentFile.state);
+    console.log('[BranchSelector] onChangeBranch',currentFile.state);
     if(currentFile.state == FileState.modified || currentFile.state == FileState.saved) {
       if(!confirm(t('ファイルが保存されていません。変更を破棄しますか?'))) {
         return;
