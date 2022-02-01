df3427ebe32c0b9838fee2d6186ae9c5282d0819
Ayumu Takai <atakai@hackwork.jp>
2022-02-02 06:36:12 +0900

    ファイルアップロード後にファイル一覧をリロードする

--- web/src/components/FileUploadModal.tsx
+++ web/src/components/FileUploadModal.tsx
@@ -3,24 +3,24 @@ import * as UI from './ui';
 import {useRepositoryContext} from '@middlewares/contexts/useRepositoryContext';
 import {User} from 'firebase/auth';
 import {useLogContext} from '@middlewares/contexts/useLogContext';
-import { useAppContext } from '@middlewares/contexts/useAppContext';
-import { gql } from '@apollo/client';
+import {useAppContext} from '@middlewares/contexts/useAppContext';
+import {gql} from '@apollo/client';
 import upath from 'upath';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
 /**
- * 
- * @param file 
- * @returns 
+ *
+ * @param file
+ * @returns
  */
- export const getBase64 = (file: File): Promise<string | ArrayBuffer | null> => {
-  const reader = new FileReader()
+export const getBase64 = (file: File): Promise<string | ArrayBuffer | null> => {
+  const reader = new FileReader();
   return new Promise((resolve, reject) => {
-    reader.onload = () => resolve(reader.result)
-    reader.onerror = error => reject(error);
-    reader.readAsDataURL(file)
-  })
-}
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+    reader.readAsDataURL(file);
+  });
+};
 
 export const FileUploadModal = ({
   user,
@@ -69,55 +69,85 @@ export const FileUploadModal = ({
       setBusy(true);
       try {
         const MAX_SIZE = 700; // Vercelによるアップロードサイズ上限(おそらく750KBあたり)
-        if(file.size > MAX_SIZE * 1024) {
-          log.error(t('ファイルサイズが大きすぎます',{MAX: MAX_SIZE,filename:file.name}), 1000);
+        if (file.size > MAX_SIZE * 1024) {
+          log.error(
+            t('ファイルサイズが大きすぎます', {
+              MAX: MAX_SIZE,
+              filename: file.name,
+            }),
+            1000,
+          );
           return;
         }
         const encodedData = (await getBase64(file))?.toString().split(',')[1];
-        const currentDir = repository.state.currentTree.map((f) => f.name).join('/');
+        const currentDir = repository.state.currentTree
+          .map((f) => f.name)
+          .join('/');
         const filePath = upath.join(currentDir, fileName);
         // console.log('upload image encodedData', encodedData);
-        if(!encodedData) {
-          log.error(t('ファイルを取得できませんでした',{filepath:filePath,error:"conent error"}), 1000);
-          return; 
+        if (!encodedData) {
+          log.error(
+            t('ファイルを取得できませんでした', {
+              filepath: filePath,
+              error: 'conent error',
+            }),
+            1000,
+          );
+          return;
         }
 
         // TODO: リポジトリコンテクストにメソッド化したほうがowner,repo,branchの指定が不要になるので良いか
-        const result = await app.gqlclient?.mutate({mutation:gql`
-          mutation createFile($owner: String!, $repo: String!, $branch: String!, $path: String!, $encodedData: String!, $message: String!) {
-            commitContent(params:{
-              owner: $owner,
-              repo: $repo,
-              branch: $branch,
-              newPath: $path,
-              newContent: $encodedData,
-              message: $message
-            }) {
-              state,
-              message
+        const result = (await app.gqlclient?.mutate({
+          mutation: gql`
+            mutation createFile(
+              $owner: String!
+              $repo: String!
+              $branch: String!
+              $path: String!
+              $encodedData: String!
+              $message: String!
+            ) {
+              commitContent(
+                params: {
+                  owner: $owner
+                  repo: $repo
+                  branch: $branch
+                  newPath: $path
+                  newContent: $encodedData
+                  message: $message
+                }
+              ) {
+                state
+                message
+              }
             }
-          }
-        `,
-          variables: { 
-            owner: repository.state.owner, 
+          `,
+          variables: {
+            owner: repository.state.owner,
             repo: repository.state.repo,
             branch: repository.state.branch,
-            path: filePath, 
+            path: filePath,
             encodedData,
-            message:"create file"
-          }
-        }) as any;
+            message: 'create file',
+          },
+        })) as any;
         // console.log('upload image result',result);
-        if(result.data.commitContent.state) {
-          log.success(t('ファイルを追加しました',{filename: file.name}), 1000);
-          repository.selectBranch(repository.state.branch!); // ファイル一覧の更新
-        }else{
-          log.error(t('ファイルを追加できませんでした', {filename: file.name}), 1000);
+        if (result.data.commitContent.state) {
+          log.success(t('ファイルを追加しました', {filename: file.name}), 1000);
+          repository.selectTree('.'); // ファイル一覧の更新
+        } else {
+          log.error(
+            t('ファイルを追加できませんでした', {filename: file.name}),
+            1000,
+          );
         }
         onClose();
       } catch (error) {
         console.error(error);
-        log.error(t('ファイルを追加できませんでした', {filename: file.name}), 1000);
+        log.error(
+          t('ファイルを追加できませんでした', {filename: file.name}),
+          1000,
+        );
       } finally {
         setBusy(false);
       }
