2e2820096dbbab271ae583aa765485b3d2260c97
Ayumu Takai <atakai@hackwork.jp>
2022-02-06 07:49:12 +0900

    fix: bug in getting directory  from query params

--- web/src/middlewares/contexts/useRepositoryContext.tsx
+++ web/src/middlewares/contexts/useRepositoryContext.tsx
@@ -229,7 +229,8 @@ export function RepositoryContextProvider({
             );
             const fs: WebApiFs = await WebApiFs.open(props);
             const dirname = filePath ? upath.dirname(filePath) : '';
-            const files = await fs.readdir(dirname);
+            const dir = dirname !== '.' ? dirname:''; // upath.dirname('sample.md') => '.' になるため、次のreaddirでルートではなくカレントディレクトリを取得してしまう。
+            const files = await fs.readdir(dir);
             let file;
             if (filePath && filePath != state.currentFile?.path) {
               const name = upath.basename(filePath);
